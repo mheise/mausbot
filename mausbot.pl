c0a0333592d38793d16af76b0a98020b6b31c543
KaeseEs <kaesees@gmail.com>
2010-01-27 21:34:54 -0500

    initial commit

---
+++ mausbot.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+# mausbot - a perl irc bot
+# Copyright (c) 2010 Mike Heise
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+use warnings;
+use strict;
