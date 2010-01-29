b54ad0ef5a807d2c154a75c527911532890e9f4c
KaeseEs <kaesees@gmail.com>
2010-01-28 22:42:15 -0500

    retabbed shit so it doesn't look retarded on github

--- Maus/Config.pm
+++ Maus/Config.pm
@@ -13,26 +13,26 @@ our @EXPORT_OK = qw(get_config);
 
 my ($config_json, $site_config_json, $config, $site_config);
 eval {
-		$config_json = file('config.json')->slurp;
-		$config = decode_json($config_json);
+        $config_json = file('config.json')->slurp;
+        $config = decode_json($config_json);
 } or do {
-		croak "Couldn't parse config file: $@";
+        croak "Couldn't parse config file: $@";
 };
 
 eval {
-		$site_config_json = file('site_config.json')->slurp;
-		$site_config = decode_json($site_config_json);
+        $site_config_json = file('site_config.json')->slurp;
+        $site_config = decode_json($site_config_json);
 } or do {
-		carp "Couldn't parse site config file: $@";
-		$site_config = {};
+        carp "Couldn't parse site config file: $@";
+        $site_config = {};
 };
 
 Hash::Merge::set_behavior('RIGHT_PRECEDENT');
 my %config = %{ merge($config, $site_config) };
 
 sub get_config {
-	my ($item) = @_;
-	return $config{$item};
+    my ($item) = @_;
+    return $config{$item};
 }
 
 1;
