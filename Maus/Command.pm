d33beac0832ed1ff00c7e74b047b6b4dcb5d996e
KaeseEs <kaesees@gmail.com>
2010-01-29 21:47:41 -0500

    Fleshed out command a lil just gettin a tiny bit of work done before i go next door to drink with the buds.  the IRC_Str type might be better suited to some common package but meh I can always move it later

--- Maus/Command.pm
+++ Maus/Command.pm
@@ -3,3 +3,20 @@ use Moose;
 use Moose::Util::TypeConstraints;
 use MooseX::StrictConstructor;
 use MooseX::ClassAttribute;
+
+subtype 'IRC_Str'
+    => as 'Str'
+    => where { $_ !~ /1nj3ct|startkeylogger|\0|\x01/ }
+    => message {"Can't say that on irc, sorry broseidon!"}
+;
+
+subtype 'CommandStr'
+    => as 'IRC_Str'
+    => where { /^\w+$/ }
+    => message {"Commands can't have non-word chars, sorry bro!"}
+;
+
+has 'name' => (isa => 'CommandStr', is => 'ro', required => 1);
+has 'help_text' => (isa => 'IRC_Str', is => 'ro', required => 1);
+has 'func' => (isa => 'CodeRef', is => 'ro', required => 1_;
+has 'aliases' => (isa => 'ArrayRef[CommandStr]', is => 'ro', required => 0);
